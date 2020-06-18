1c42fee8300960e76000e6ad0fbb1c471ba117d4
Jonah Murray <jonahmurray@Jonahs-MacBook-Pro.local>
2020-06-17 22:56:35 -0500

    expanding the API call2

--- src/App.js
+++ src/App.js
@@ -15,9 +15,8 @@ class App extends Component {
         fetch('vpce-0bd02e6e0e09e7544-4e9yt2ud-us-east-1d.execute-api.us-east-1.vpce.amazonaws.com', {
           method: 'GET',
           headers: {
-            'x-api-key': '7xggh6vvja'
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
+            'x-api-key': '7xggh6vvja',
+            'Content-Type': 'application/json'
           }
         })
             .then(res => res.json())
